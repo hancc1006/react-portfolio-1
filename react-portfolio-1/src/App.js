b3e1d12aade8cf9b4d6a3393f5d573794f5e3286
Crystal Han <hancc1006@gmail.com>
2021-11-10 23:12:32 -0800

    Starting react app

---
+++ react-portfolio-1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <p>React App</p>
+    </div>
+  );
+}
+
+export default App;
